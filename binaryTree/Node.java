fe76e1dfc35857b208eeb65202ebce31d3521299
Dhruv Parthasarathy <54674314+DhruvParthasarathy@users.noreply.github.com>
2022-10-31 19:57:23 -0400

    Create Node.java

---
+++ binaryTree/Node.java
@@ -0,0 +1,16 @@
+package binaryTree;
+
+public class Node {
+
+    char value;
+    Node left;
+    Node right;
+
+    Node(char value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+
+}
